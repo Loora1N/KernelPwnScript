a93377b171386c8dce1b33a3ba726ada58de1feb
Loora1N <15129303223@163.com>
2023-11-17 13:38:38 +0800

    add symbol function for gdb_kernel.sh

--- sh/gdb_kernel.sh
+++ sh/gdb_kernel.sh
@@ -3,11 +3,13 @@
 # Script Name: unfile.sh
 # Author: Loora1N
 # Update Date: 2023-11-15
-# Version: 0.6
+# Version: 0.7
 # Description: This script is designed for gdb kernel
 
-
-#TODO:自定义程度太低
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 <vmlinux_file> [-s|--symbol <path> <offset>]"
+}
 
 if [ -z "$1" ]; then
     echo "Error: please provide the file as argument."
@@ -16,8 +18,24 @@ if [ -z "$1" ]; then
 fi
 
 vmlinux="${1}"
+symbol_flag=false
 
-gdb "$vmlinux"\
-    -ex 'target remote localhost:1234' 
-    # -ex 'add-symbol-file ./core/fs/core.ko offset'\
-    # -ex 'b breakpoint'\
+if [[ "$2" == "--symbol" || "$2" == "-s" ]]; then
+    if [ -z "$3" ] || [ -z "$4" ]; then
+        echo "Error: Please provide both path and offset after --symbol or -s."
+        usage
+        exit 1
+    fi
+    path="$3"
+    offset="$4"
+    symbol_flag=true
+fi
+
+#TODO: 增加break point功能
+
+gdb_cmd="gdb $vmlinux -ex 'target remote localhost:1234'"
+if [ "$symbol_flag" = true ]; then
+    gdb_cmd+=" -ex 'add-symbol-file $path $offset'"
+fi
+
+eval "$gdb_cmd"
